dc96138e710bd6cb2b5de49f5304be749107c137
hamzael94-gif <elyousfiha68@gmail.com>
2023-10-22 18:04:42 +0100

    first push

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import * as RecipeAPI from './recipe-api'
+
+const app =  express();
+
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/api/recipes/search", async (req, res)=>{
+    const searchTerm = req.query.searchTerm as string;
+    const page = parseInt(req.query.page as string);
+    const results  = await RecipeAPI.searchRecipes(searchTerm,page);
+
+    return res.json(results);
+});
+
+app.get("/api/recipres/:recipeId/summary", async (req, res)=>{
+    const recipeId = req.params.recipeId;
+    const results = await RecipeAPI.getRecipeSummmary(recipeId);
+    return res.json(results);
+})
+
+app.listen(5000,()=>{
+    console.log("server running on localhost:5000");
+});
---
+++ frontend/src/components/RecipeCard.tsx
@@ -0,0 +1,18 @@
+import { Recipe } from '../types'
+
+interface Props{
+    recipe : Recipe;
+    onClick : () => void;
+}
+const RecipeCard = ({recipe, onClick}: Props) => {
+  return (
+    <div className='recipe-card' onClick={onClick}>
+      <img src={recipe.image}/>
+      <div className='recipe-card-title'>
+        <h3>{recipe.title}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default RecipeCard
